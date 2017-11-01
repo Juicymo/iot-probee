8a5bb53105cbb36c852a64daae7f0c2d41f575c2
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-01 14:35:23 +0100

    Refactor ultrasonic code to use the NewPing library
    The NewPing library is using a hardware Timer2 on Arduino - that will disable the PWM functionality on pins 9 and 11 on Arduino Mega. Make sure you either do not use those pins at all, or that you do not use them as PWM pins.

--- arduino/arduino-probee/src/hal/hal_sensors_ultrasonic.h
+++ arduino/arduino-probee/src/hal/hal_sensors_ultrasonic.h
@@ -1,41 +1,57 @@
+// Variables
+unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
+unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
+uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+
+NewPing sonar[SONAR_NUM] = {              // Sensor object array.
+  NewPing(SONAR_TURRET_TRIGGER, SONAR_TURRET_ECHO, SONAR_MAX_DISTANCE),    // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(SONAR_FORWARD_TRIGGER, SONAR_FORWARD_ECHO, SONAR_MAX_DISTANCE),
+  NewPing(SONAR_BACKWARD_TRIGGER, SONAR_BACKWARD_ECHO, SONAR_MAX_DISTANCE)
+};
+
 // Private
-float hal_sensors_ultrasonic_perform_measure(byte pin_trigger, byte pin_echo) {
-  digitalWrite(pin_trigger, LOW);
-  delayMicroseconds(2);
+void _echoCheck() { // If ping received, set the sensor distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
 
-  digitalWrite(pin_trigger, HIGH);
-  delayMicroseconds(10);
-
-  digitalWrite(pin_trigger, LOW);
-  float duration = pulseIn(pin_echo, HIGH, 30 * 1000);
-
-  // Calculate the distance (in cm) based on the speed of sound.
-  float distance = duration / 58.2;
-
-  return distance;
+void _oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
+  // The following code would be replaced with your code that does something with the ping results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+  }
+  Serial.println();
 }
 
 // Public
 void setup_hal_sensors_ultrasonic() {
-	// Turret
-	pinMode(SONAR_TURRET_TRIGGER, OUTPUT);
-	pinMode(SONAR_TURRET_ECHO, INPUT);
-	// Forward
-	pinMode(SONAR_FORWARD_TRIGGER, OUTPUT);
-	pinMode(SONAR_FORWARD_ECHO, INPUT);
-	// Backward
-	pinMode(SONAR_BACKWARD_TRIGGER, OUTPUT);
-	pinMode(SONAR_BACKWARD_ECHO, INPUT);
+	// // Turret
+	// pinMode(SONAR_TURRET_TRIGGER, OUTPUT);
+	// pinMode(SONAR_TURRET_ECHO, INPUT);
+	// // Forward
+	// pinMode(SONAR_FORWARD_TRIGGER, OUTPUT);
+	// pinMode(SONAR_FORWARD_ECHO, INPUT);
+	// // Backward
+	// pinMode(SONAR_BACKWARD_TRIGGER, OUTPUT);
+	// pinMode(SONAR_BACKWARD_ECHO, INPUT);
+
+  pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
+  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
+    pingTimer[i] = pingTimer[i - 1] + SONAR_PING_INTERVAL;
 }
 
-float hal_sensors_ultrasonic_turret_scan() {
-	return hal_sensors_ultrasonic_perform_measure(SONAR_TURRET_TRIGGER, SONAR_TURRET_ECHO);
-}
-
-float hal_sensors_ultrasonic_forward_scan() {
-	return hal_sensors_ultrasonic_perform_measure(SONAR_FORWARD_TRIGGER, SONAR_FORWARD_ECHO);
-}
-
-float hal_sensors_ultrasonic_backward_scan() {
-	return hal_sensors_ultrasonic_perform_measure(SONAR_BACKWARD_TRIGGER, SONAR_BACKWARD_ECHO);
+void loop_hal_sensors_ultrasonic() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
+      pingTimer[i] += SONAR_PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
+      if (i == 0 && currentSensor == SONAR_NUM - 1) _oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
+      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
+      currentSensor = i;                          // Sensor being accessed.
+      cm[currentSensor] = 0;                      // Make distance zero in case there's no ping echo for this sensor.
+      sonar[currentSensor].ping_timer(_echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
+    }
+  }
 }
