9a6a6ba4f6ebdf73af8992421e57e6e2f64c2f14
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 21:08:52 +0100

    Add transition helpers to FSM macros

--- arduino/arduino-probee/src/macros/macros_fsm.h
+++ arduino/arduino-probee/src/macros/macros_fsm.h
@@ -8,6 +8,9 @@
 #define STATE_UPDATE(name)  void _on_state_##name##_update()
 #define STATE_EXIT(name)    void _on_state_##name##_exit()
 
+#define TRANSITION(fsm_name, from, to, event)             fsm_##fsm_name.add_transition(&state_##from, &state_##to, event, NULL)
+#define TIMED_TRANSITION(fsm_name, from, to, interval)    fsm_##fsm_name.add_timed_transition(&state_##from, &state_##to, interval, NULL)
+
 #define INIT_FSM(name, default_state)    Fsm fsm_##name(&state_##default_state)
 
 #define UPDATE_FSM(name)    fsm_##name.run_machine()
