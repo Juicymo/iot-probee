286d50dec09c62fb3aa662512607b1ddccc3d431
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 10:42:35 +0100

    Enable AI layer in multitasking

--- arduino/arduino-probee/src/multitasking.h
+++ arduino/arduino-probee/src/multitasking.h
@@ -1,21 +1,21 @@
 // Private
 //DEFINE_TASK(hal_bluetooth);
-//DEFINE_TASK(state_machine);
 //DEFINE_TASK(hal_i2c);
+DEFINE_TASK(ai);
 DEFINE_TASK(hal_servo);
 
 // Public
 void setup_multitasking() {
     //INIT_TASK(hal_bluetooth);
-    //INIT_TASK(state_machine);
     //INIT_TASK(hal_i2c);
+    INIT_TASK(ai);
     INIT_TASK(hal_servo);
 }
 
 void loop_multitasking() {
     //PERFORM_TASK(hal_bluetooth, TICK_BLUETOOTH);
-    //PERFORM_TASK(state_machine, TICK_STATE_MACHINE);
     //PERFORM_TASK(hal_i2c, TICK_I2C);
     PERFORM_TASK(hal_servo, TICK_SCANNING);
+    PERFORM_TASK(ai, TICK_AI);
     PERFORM_TASK_ALWAYS(loop_hal_sensors_ultrasonic);
 }
