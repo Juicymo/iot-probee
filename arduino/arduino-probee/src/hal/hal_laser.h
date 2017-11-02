ae6cf347e5748439703c04b1ae9fa8505a2c2f4a
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 23:32:25 +0100

    Add function for status detection to laser HAL

--- arduino/arduino-probee/src/hal/hal_laser.h
+++ arduino/arduino-probee/src/hal/hal_laser.h
@@ -1,15 +1,27 @@
 // Private
+boolean _hal_laser_turret_on = false;
 
 // Public
 void hal_laser_on(byte laser) {
 	if (laser == LASER_TURRET) {
+		_hal_laser_turret_on = true;
 		digitalWrite(LASER_TURRET_PIN, HIGH);
 	}
 }
 
 void hal_laser_off(byte laser) {
 	if (laser == LASER_TURRET) {
+		_hal_laser_turret_on = false;
 		digitalWrite(LASER_TURRET_PIN, LOW);
+	}
+}
+
+boolean hal_laser_is_on(byte laser) {
+	if (laser == LASER_TURRET) {
+		return _hal_laser_turret_on;
+	}
+	else {
+		return false;
 	}
 }
 
