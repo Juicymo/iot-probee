3afe450e5ed19e2deab81fb16dc6217c5ba54edb
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 23:33:11 +0100

    Add direction remembering and braking ability to motor HAL

--- arduino/arduino-probee/src/hal/hal_motor.h
+++ arduino/arduino-probee/src/hal/hal_motor.h
@@ -1,4 +1,7 @@
 // Private
+boolean _motor_left_direction 	= MOTOR_DIRECTION_FORWARD; // true = forward, false = backward
+boolean _motor_right_direction 	= MOTOR_DIRECTION_FORWARD; // true = forward, false = backward
+
 void _hal_motor_off(byte motor) {
 	if (motor == MOTOR_LEFT || motor == MOTOR_BOTH) {
 		analogWrite(MOTOR_LEFT_PWM_PIN, 0);
@@ -14,11 +17,13 @@ void _hal_motor_off(byte motor) {
 
 void _hal_motor_drive_fwd(byte motor, byte pwm) {
 	if (motor == MOTOR_LEFT || motor == MOTOR_BOTH) {
+		_motor_left_direction = MOTOR_DIRECTION_FORWARD;
 		analogWrite(MOTOR_LEFT_PWM_PIN, pwm);
 		digitalWrite(MOTOR_LEFT_DIRECTION_PIN, HIGH);
 		digitalWrite(MOTOR_LEFT_ENABLE_PIN, HIGH);
 	}
 	if (motor == MOTOR_RIGHT || motor == MOTOR_BOTH) {
+		_motor_right_direction = MOTOR_DIRECTION_FORWARD;
 		analogWrite(MOTOR_RIGHT_PWM_PIN, pwm);
 		digitalWrite(MOTOR_RIGHT_DIRECTION_PIN, HIGH);
 		digitalWrite(MOTOR_RIGHT_ENABLE_PIN, HIGH);
@@ -27,13 +32,42 @@ void _hal_motor_drive_fwd(byte motor, byte pwm) {
 
 void _hal_motor_drive_bwd(byte motor, byte pwm) {
 	if (motor == MOTOR_LEFT || motor == MOTOR_BOTH) {
+		_motor_left_direction = MOTOR_DIRECTION_BACKWARD;
 		analogWrite(MOTOR_LEFT_PWM_PIN, pwm);
 		digitalWrite(MOTOR_LEFT_DIRECTION_PIN, LOW);
+		digitalWrite(MOTOR_LEFT_ENABLE_PIN, HIGH);
+	}
+	if (motor == MOTOR_RIGHT || motor == MOTOR_BOTH) {
+		_motor_right_direction = MOTOR_DIRECTION_BACKWARD;
+		analogWrite(MOTOR_RIGHT_PWM_PIN, pwm);
+		digitalWrite(MOTOR_RIGHT_DIRECTION_PIN, LOW);
+		digitalWrite(MOTOR_RIGHT_ENABLE_PIN, HIGH);
+	}
+}
+
+void _hal_motor_brake(byte motor, byte pwm) {
+	if (motor == MOTOR_LEFT || motor == MOTOR_BOTH) {
+		analogWrite(MOTOR_LEFT_PWM_PIN, pwm);
+
+		if (_motor_left_direction == MOTOR_DIRECTION_FORWARD) {
+			digitalWrite(MOTOR_LEFT_DIRECTION_PIN, LOW);
+		}
+		else {
+			digitalWrite(MOTOR_LEFT_DIRECTION_PIN, HIGH);
+		}
+
 		digitalWrite(MOTOR_LEFT_ENABLE_PIN, LOW);
 	}
 	if (motor == MOTOR_RIGHT || motor == MOTOR_BOTH) {
 		analogWrite(MOTOR_RIGHT_PWM_PIN, pwm);
-		digitalWrite(MOTOR_RIGHT_DIRECTION_PIN, LOW);
+
+		if (_motor_right_direction == MOTOR_DIRECTION_FORWARD) {
+			digitalWrite(MOTOR_RIGHT_DIRECTION_PIN, LOW);
+		}
+		else {
+			digitalWrite(MOTOR_RIGHT_DIRECTION_PIN, HIGH);
+		}
+
 		digitalWrite(MOTOR_RIGHT_ENABLE_PIN, LOW);
 	}
 }
@@ -71,6 +105,12 @@ void hal_motor_turn_left(float speed) {
 void hal_motor_turn_right(float speed) {
 	hal_motor_drive(MOTOR_LEFT, -speed);
 	hal_motor_drive(MOTOR_RIGHT, speed);
+}
+
+void hal_motor_brake(byte motor) {
+	float speed = MOTOR_SPEED_QUATER;
+	byte pwm = abs(speed) * 255;
+	_hal_motor_brake(motor, pwm);
 }
 
 void hal_motor_stop(byte motor) {
