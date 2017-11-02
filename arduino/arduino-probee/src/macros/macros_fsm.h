b58f3b639d1bc0cdda555736d01d0e2fa9540c89
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 10:32:46 +0100

    Refactor FSM macros

--- arduino/arduino-probee/src/macros/macros_fsm.h
+++ arduino/arduino-probee/src/macros/macros_fsm.h
@@ -1,9 +1,13 @@
 // Macros
-#define NEW_STATE(name)     void name##_enter();  \
-                            void name##_update(); \
-                            void name##_exit();   \
-                            State name = State(name##_enter, name##_update, name##_exit);
+#define DEFINE_STATE(name)  void _on_state_##name##_enter();  \
+                            void _on_state_##name##_update(); \
+                            void _on_state_##name##_exit();   \
+                            State state_##name(&_on_state_##name##_enter, &_on_state_##name##_update, &_on_state_##name##_exit);
 
-#define STATE_ENTER(name)   void name##_enter()
-#define STATE_UPDATE(name)  void name##_update()
-#define STATE_EXIT(name)    void name##_exit()
+#define STATE_ENTER(name)   void _on_state_##name##_enter()
+#define STATE_UPDATE(name)  void _on_state_##name##_update()
+#define STATE_EXIT(name)    void _on_state_##name##_exit()
+
+#define INIT_FSM(name, default_state)    Fsm fsm_##name(&state_##default_state)
+
+#define UPDATE_FSM(name)    fsm_##name.run_machine()
