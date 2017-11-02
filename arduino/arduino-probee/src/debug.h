feaa1f55ebfb76c13d3fe6aff6210065156230ae
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 10:47:30 +0100

    Add support for warnings and errors in debug

--- arduino/arduino-probee/src/debug.h
+++ arduino/arduino-probee/src/debug.h
@@ -2,9 +2,13 @@
 #ifdef PROBEE_DEBUG
 #define TRACE(x) Serial.print(x);
 #define TRACELN(x) Serial.println(x);
+#define WARNING(x) Serial.print("Warning: "); Serial.println(x);
+#define ERROR(x) Serial.print("Error: "); Serial.println(x);
 #else
 #define TRACE(x)
 #define TRACELN(x)
+#define WARNING(x)
+#define ERROR(x)
 #endif
 
 // Private
