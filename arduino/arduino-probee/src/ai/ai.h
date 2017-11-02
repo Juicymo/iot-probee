8e5bf9992219caa8d7427dd97432bda2ca60b576
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-02 10:42:09 +0100

    Enable FSM movement as an AI layer

--- arduino/arduino-probee/src/ai/ai.h
+++ arduino/arduino-probee/src/ai/ai.h
@@ -1,5 +1,14 @@
 // Includes
-#include "state_machine.h"
+#include "fsm_movement.h"
 //#include "blackboard.h"
 //#include "environment.h"
 //#include "mas.h"
+
+// Public
+void setup_ai() {
+	setup_fsm_movement();
+}
+
+void ai_loop() {
+	fsm_movement_loop();
+}
